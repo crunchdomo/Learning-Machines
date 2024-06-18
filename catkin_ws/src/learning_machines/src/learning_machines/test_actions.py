67bb79afd5d1be853b69978ed9ee4339e7af7ce0
crunchdomo <oenfadam@gmail.com>
2024-06-18 14:13:52 +0200

    PPO Init

--- catkin_ws/src/learning_machines/src/learning_machines/test_actions.py
+++ catkin_ws/src/learning_machines/src/learning_machines/test_actions.py
@@ -2,42 +2,41 @@
 
 import gym
 import torch
-from stable_baselines3 import TD3  
+from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv, VecCheckNan
-from stable_baselines3.common.noise import NormalActionNoise
+from stable_baselines3.common.callbacks import StopTrainingOnMaxEpisodes
 from robobo_interface import SimulationRobobo, IRobobo
 import numpy as np
-from collections import deque, namedtuple
+from collections import deque
 from data_files import FIGRURES_DIR
 
 class RoboboEnv(gym.Env):
     def __init__(self, rob: IRobobo):
         super(RoboboEnv, self).__init__()
         self.rob = rob
-        self.action_space = gym.spaces.Box(low=-1, high=1, shape=(2,), dtype=np.float32)  
+        self.action_space = gym.spaces.Box(low=-1, high=1, shape=(2,), dtype=np.float32)
         self.observation_space = gym.spaces.Box(low=0, high=1, shape=(8,), dtype=np.float32)
-        self.recent_actions = deque(maxlen=30)  
-        self.total_reward = 0  
+        self.recent_actions = deque(maxlen=30)
+        self.total_reward = 0
 
     def reset(self):
         self.rob.play_simulation()
         self.recent_actions.clear()
-        self.total_reward = 0  
+        self.total_reward = 0
         return self.get_state()
 
     def step(self, action):
         state = self.get_state()
-        left_speed = action[0] * 100  
-        right_speed = action[1] * 100  
+        left_speed = action[0] * 100
+        right_speed = action[1] * 100
 
         self.rob.move_blocking(left_speed, right_speed, 100)
         next_state = self.get_state()
         reward = self.get_reward(action, next_state)
-        self.total_reward += reward  
+        self.total_reward += reward
         done = self.is_done(next_state, reward)
-        self.recent_actions.append(tuple(action)) 
+        self.recent_actions.append(tuple(action))
 
-        # Debugging logs
         print(f"Action: {action}, Left Speed: {left_speed}, Right Speed: {right_speed}")
         print(f"State: {state}, Next State: {next_state}, Reward: {reward}, Done: {done}")
 
@@ -45,22 +44,20 @@ class RoboboEnv(gym.Env):
 
     def get_state(self):
         ir_values = self.rob.read_irs()
-        ir_values = np.clip(ir_values, 0, 10000)  
-        ir_values = ir_values / 10000.0  
+        ir_values = np.clip(ir_values, 0, 10000)
+        ir_values = ir_values / 10000.0
         if np.any(np.isinf(ir_values)) or np.any(np.isnan(ir_values)):
             print("Invalid IR sensor values detected:", ir_values)
         return np.array(ir_values, dtype=np.float32)
 
     def get_reward(self, action, state):
-        if np.any(state > 1.0):  
-            return 0
+        if np.any(state > 0.7):
+            return -10
 
         reward = 20 * (action[0] + action[1])
-        reward -= 10 * (1 - (action[0] + action[1])) 
 
-
-        if self.recent_actions.count(tuple(action)) > 2: 
-            reward -= 10 
+        if self.recent_actions.count(tuple(action)) > 5:
+            reward -= 10
 
         if len(self.recent_actions) > 1 and tuple(action) != self.recent_actions[-1]:
             reward += 1
@@ -85,32 +82,18 @@ class RoboboEnv(gym.Env):
 robobo_instance = SimulationRobobo()
 
 env = DummyVecEnv([lambda: RoboboEnv(robobo_instance)])
-env = VecCheckNan(env, raise_exception=True)  
+env = VecCheckNan(env, raise_exception=True)
 
+model = PPO('MlpPolicy', env, verbose=1)
 
-n_actions = env.action_space.shape[-1]
-action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
+max_episodes = 10
 
-model = TD3('MlpPolicy', env, verbose=1)
+callback = StopTrainingOnMaxEpisodes(max_episodes=max_episodes, verbose=1)
 
+model.learn(total_timesteps=1, callback=callback)  
 
-model.learn(total_timesteps=10000)
-
-
-model.save("td3_robobo")
-
-model = TD3.load("td3_robobo")
+save_location = FIGRURES_DIR / "ppo_robobo.zip"
+print(f'saving robo to: {save_location}')
+model.save(save_location)
 
 obs = env.reset()
-total_rewards = [] 
-for _ in range(1000):
-    action, _states = model.predict(obs, deterministic=True)
-    obs, reward, done, info = env.step(action)
-    print(reward)
-    total_rewards.append(reward)  # Track the reward
-    if done:
-        break
-    env.render()
-
-# Print the total rewards for each episode
-print("Total rewards for each episode:", total_rewards)
