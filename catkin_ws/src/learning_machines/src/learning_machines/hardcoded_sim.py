d5ecebe9f1739cf208b5eac742a88ed0aa8fe4af
Daniel vH <djvondan@gmail.com>
2024-06-18 10:53:15 +0200

    camera fix for sim on demo

--- catkin_ws/src/learning_machines/src/learning_machines/hardcoded_sim.py
+++ catkin_ws/src/learning_machines/src/learning_machines/hardcoded_sim.py
@@ -104,18 +104,20 @@ def test_move_and_wheel_reset(rob):
 def test_sensors(rob: IRobobo):
     print("IRS data: ", rob.read_irs())
     image = rob.get_image_front()
+    if isinstance(rob, SimulationRobobo):
+        image = cv2.flip(image, 0)
     cv2.imwrite(str(FIGRURES_DIR / "photo.png"), image)
     print("Phone pan: ", rob.read_phone_pan())
     print("Phone tilt: ", rob.read_phone_tilt())
     print("Current acceleration: ", rob.read_accel())
     print("Current orientation: ", rob.read_orientation())
 
-
 def test_phone_movement(rob: IRobobo):
-    rob.set_phone_pan_blocking(20, 100)
-    print("Phone pan after move to 20: ", rob.read_phone_pan())
-    rob.set_phone_tilt_blocking(50, 100)
+    rob.set_phone_tilt_blocking(100, 100)
     print("Phone tilt after move to 50: ", rob.read_phone_tilt())
+    time.sleep(1)
+    rob.set_phone_tilt_blocking(100, 100)
+    time.sleep(1)
 
 
 def test_sim(rob: SimulationRobobo):
@@ -151,3 +153,6 @@ def run_all_actions(rob: IRobobo):
 
     if isinstance(rob, SimulationRobobo):
         rob.stop_simulation()
+
+
+
